270b8749e1a1707696a12bab8b4cb1bb045d3669
RashiShekhar <rashi1shekhar@email.com>
2025-08-21 21:41:47 +0530

    login page logic done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 export default function Login() {
   const navigate = useNavigate();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:5000/api/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        login(data.user);
+        nav("/dashboard");
+      } else {
+        alert("Invalid credentials");
+      }
+    } catch (error) {
+      console.error("Login error:", error);
+      alert("Server error during login");
+    }
+  };
   return (
     <div className="min-h-screen flex">
       <div className="hidden md:flex w-1/2 relative overflow-hidden">
@@ -15,19 +41,15 @@ export default function Login() {
           <h2 className="text-3xl font-bold text-gray-800 mb-6 text-center">
             Welcome Back
           </h2>
-          <form
-            className="space-y-5"
-            onSubmit={(e) => {
-              e.preventDefault(); // stop page reload
-              navigate("/dashboard"); // navigate on successful login
-            }}
-          >
+          <form className="space-y-5" onSubmit={handleLogin}>
             <div>
               <label className="block mb-1 text-gray-600">Email</label>
               <input
                 type="email"
                 className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                 placeholder="Enter your email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
               />
             </div>
             <div>
@@ -36,6 +58,8 @@ export default function Login() {
                 type="password"
                 className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                 placeholder="Enter your password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
             <button
