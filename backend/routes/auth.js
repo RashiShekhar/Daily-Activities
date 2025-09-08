045dd14b34f37dcf9c7265e5c0f859e8996787b3
RashiShekhar <rashi1shekhar@email.com>
2025-09-08 20:59:01 +0530

    login page done

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -14,14 +14,46 @@ router.post("/signup", async (req, res) => {
     const hashed = await bcrypt.hash(password, 10);
     const user = await User.create({ name, email, password: hashed });
 
-    res
-      .status(201)
-      .json({
-        message: "User created",
-        user: { name: user.name, email: user.email },
-      });
+    res.status(201).json({
+      message: "User created",
+      user: { name: user.name, email: user.email },
+    });
   } catch (err) {
     console.error(err);
     res.status(500).json({ message: "Server error during signup" });
+  }
+});
+
+//login
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    res.status(200).json({
+      message: "Successfully logged in",
+      user: {
+        name: existingUser.name,
+        email: existingUser.email,
+        // _id: existingUser._id, // You can include this too
+      },
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error during login" });
   }
 });
